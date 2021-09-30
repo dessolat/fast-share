799645be4a9c227da2ffc253b97ab6dffe8b011b
Igor Nikiforov <desola2@inbox.ru>
2021-09-30 16:27:50 +0300

    Add view engine, add get route
    * Add view page code-display.ejs
    
    * Modify server.js  - add get route '/'

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.get('/', (req, res) => {
+  const code = 'A code will be here...';
+  res.render('code-display', { code });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
