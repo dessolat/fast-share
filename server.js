955fbd9ff6ea7c4ae94a87b1ccea440c9713c530
Igor Nikiforov <desola2@inbox.ru>
2021-09-30 17:45:42 +0300

    Modify code-display.ejs, add styles.css, public
    * Add line numbers column to code-display.ejs
    
    * Create styles.css, add styles
    
    * Create 'public' directory
    
    * Create welcome.js with welcome text

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const code = require('./welcome')
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  const code = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repellendus, quae reprehenderit autem quisquam exercitationem corrupti ea pariatur dolorum, unde velit ab facilis. Reprehenderit architecto omnis odit perspiciatis eaque blanditiis.
-	Autem excepturi aut minus molestiae corrupti voluptatem et. Corrupti non aliquam, consequuntur nihil dolore quidem eaque blanditiis exercitationem voluptatum asperiores eum saepe natus adipisci a repellat hic officia. Exercitationem, itaque?
-	Enim deserunt molestias, minima ullam ea similique, reprehenderit impedit error nemo laudantium ut ducimus saepe voluptatum est sapiente eveniet placeat et ipsa hic iusto a sint quos. Ipsa, id suscipit?
-	Quod dignissimos eligendi earum mollitia? Vero magnam possimus suscipit nostrum fugit soluta cum commodi quibusdam minus eveniet, minima reiciendis odio cupiditate rerum porro nihil iste. Laborum modi unde excepturi illum!`;
-  res.render('code-display', { code });
+	res.render('code-display', { code });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ welcome.js
@@ -0,0 +1,4 @@
+module.exports = `	Welcome to the Fast-share!
+	
+Use the buttons in the top-right corner
+to create a new file and share with others.`
